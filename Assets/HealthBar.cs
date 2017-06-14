5e529b2a841f35853fc6f48fdec34324078ec492
dmdSpirit <doomedspirit.prog@gmail.com>
2017-06-14 14:11:24 +0300

    Health bar manipulation

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class HealthBar : MonoBehaviour {
 	public Transform unitTransform;
+
+	[Range(0,1)]
+	public float healthPercent = 1f;
+
+	RectTransform healthImageRectTransform; 
+
 	Canvas c;
 	void Start(){
 		c = transform.parent.GetComponent<Canvas> ();
+
+		// FIXME: Check if null.
+		healthImageRectTransform = transform.Find ("HealthImage").GetComponent<RectTransform>();
+		HealthChanged (healthPercent);
 	}
 
 	void Update(){
@@ -15,7 +25,15 @@ public class HealthBar : MonoBehaviour {
 		newPos.z = oldPos.z;
 		newPos.x = newPos.x * c.pixelRect.width - c.pixelRect.width * 0.5f;
 		newPos.y = newPos.y * c.pixelRect.height - c.pixelRect.height * 0.5f;
-		Debug.Log (newPos);
+		//Debug.Log (newPos);
 		GetComponent<RectTransform> ().localPosition = newPos;
+
+
+
+	}
+
+	// Change health bar.
+	public void HealthChanged(float newHealthPercent){
+		healthImageRectTransform.offsetMax = new Vector2((healthPercent-1)*100,0);
 	}
 }
