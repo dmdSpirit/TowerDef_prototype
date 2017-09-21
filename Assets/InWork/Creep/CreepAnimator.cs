02464f8a376d6224bb94990dcf819d018f1ad4de
dmdSpirit <doomedspirit.prog@gmail.com>
2017-09-21 17:26:43 +0300

    Creep

---
+++ Assets/InWork/Creep/CreepAnimator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreepAnimator : MonoBehaviour {
+	public float maxRadius = 20;
+	public float currentRadius = 0;
+	public float spreadSpeed = 2;
+
+	void Update () {
+		if(currentRadius < maxRadius){
+			currentRadius += spreadSpeed * Time.deltaTime;
+			currentRadius = Mathf.Min (maxRadius, currentRadius);
+			Vector3 newSize = new Vector3 (currentRadius, transform.localScale.y, currentRadius);
+			transform.localScale = newSize;
+		}
+	}
+}
---
+++ Assets/InWork/Creep/CreepController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreepController : MonoBehaviour {
+
+	void OnTriggerEnter (Collider collider){
+		Debug.Log (collider.name + " is in the creep.");
+	}
+}
